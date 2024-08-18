6a1806af93e357c79a8bb621415349f3baf8fdcd
vikas <rawatvikas689@gmail.com>
2024-08-18 20:23:09 +0530

    commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -103,7 +103,7 @@ function Work() {
               )sdfsdf
           )}
         </div>sdfgsfgfdsdfdsf
-      </div>sdfsdsdfssdfdfsdafdasfds
+      </div>sdfsafsdfdsdfssdfdfsdafdasfds
     </div>sdsadfsdf
   );
 }
