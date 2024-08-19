f3e9686243346a3c820296f4cefd06e334a20287
vikas <rawatvikas689@gmail.com>
2024-08-19 18:16:09 +0530

    commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -105,7 +105,7 @@ function Work() {
         </div>
       </div>adf
     </div>adfadadfadasafadfdffadsfaadfdfaasddfadf
-  );adfadfadfadfsfddf
+  );adfadfadfadfsfddfadf
 }asdfasdfadf
 sadf
 export default Work;
